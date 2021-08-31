17db55017c59daad75fca29b65774403abc2e332
y-okumura <y-okumura@isp.co.jp>
2021-08-31 18:35:25 +0900

    Have `path_start_time_ns_` and `path_dead_line_ns_`

--- src/pathnode_sample/src/sample_relay_with_path.cpp
+++ src/pathnode_sample/src/sample_relay_with_path.cpp
@@ -19,7 +19,7 @@ namespace pathnode_sample
 
 // TODO: define per path
 const std::string IS_FIRST = "is_first";
-
+const std::string PATH_DEADLINE_NS = "path_deadline_ns";
 
 // Create a Talker class that subclasses the generic rclcpp::Node base class.
 // The main function below will instantiate the class as a ROS node.
@@ -30,16 +30,17 @@ public:
       : PathNode("relay", options)
   {
     declare_parameter(IS_FIRST, false);
+    declare_parameter(PATH_DEADLINE_NS, 10 * 1000 * 1000);
 
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
     auto callback =
       [this](std_msgs::msg::String::UniquePtr msg) -> void
       {
-        auto is_first = get_parameter(IS_FIRST).get_value<bool>();
+        const auto is_first = get_parameter(IS_FIRST).get_value<bool>();
         if(is_first) {
           // TODO: set now
           auto msg = std::make_unique<std_msgs::msg::String>();
-          msg->data = "1234";
+          msg->data = std::to_string(now_ns());
           path_info_pub_->publish(std::move(msg));
         }
 
@@ -65,34 +66,48 @@ public:
 
     pathname_ = "path";  // actually, topics are mapped to path
 
-    auto path_info_callback =
-        [this](std_msgs::msg::String::UniquePtr msg) -> void
-        {
-          this->path_dead_line_ = std::stoi(msg->data);
-        };
-    path_info_sub_ = this->create_subscription<std_msgs::msg::String>(
-        pathname_ + "_info", qos, path_info_callback);
-    path_info_pub_ = this->create_publisher<std_msgs::msg::String>(pathname_ + "_info", qos);
+    const auto is_first = get_parameter(IS_FIRST).get_value<bool>();
+    if(is_first) {
+      path_info_pub_ = this->create_publisher<std_msgs::msg::String>(pathname_ + "_info", qos);
+    } else {
+      auto path_info_callback =
+          [this](std_msgs::msg::String::UniquePtr msg) -> void
+          {
+            this->path_start_ns_ = std::stod(msg->data);
+            std::cout << "get path_info: " << this->path_start_ns_ << std::endl;
+          };
+      path_info_sub_ = this->create_subscription<std_msgs::msg::String>(
+          pathname_ + "_info", qos, path_info_callback);
+    }
   }
 
 private:
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
 
+  // TODO: have multiple path
   std::string pathname_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr path_info_pub_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr path_info_sub_;
 
   // TODO: have multiple times
   rclcpp::Time path_start_time_;
-  // TODO: int is right?
-  int path_dead_line_;
+  double path_start_ns_;
+  double path_dead_line_ns_;
 
-  bool exceeds_deadline(const std::string& path)
+  bool exceeds_deadline(const std::string& path) const
   {
     (void)path;
-    std::cout << "check now and " << std::to_string(path_dead_line_) << std::endl;
+    std::cout << "now: " << now_ns()
+              << " path_start: " << path_start_ns_
+              << " deadline: " << std::to_string(path_dead_line_ns_) << std::endl;
     return false;
+  }
+
+  double now_ns() const
+  {
+    rclcpp::Clock ros_clock(RCL_ROS_TIME);
+    return ros_clock.now().nanoseconds();
   }
 };
 
