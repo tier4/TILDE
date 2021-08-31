4650b130a4415dc913c03a1f4b7db94701c1784e
y-okumura <y-okumura@isp.co.jp>
2021-08-31 15:13:05 +0900

    Add relay

---
+++ src/pathnode_sample/src/sample_relay.cpp
@@ -0,0 +1,45 @@
+#include <chrono>
+#include <cstdio>
+#include <memory>
+#include <utility>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+
+#include "std_msgs/msg/string.hpp"
+
+#include "pathnode/path_node.hpp"
+
+using namespace std::chrono_literals;
+
+namespace pathnode_sample
+{
+// Create a Talker class that subclasses the generic rclcpp::Node base class.
+// The main function below will instantiate the class as a ROS node.
+class Relay : public pathnode::PathNode
+{
+public:
+  explicit Relay(const rclcpp::NodeOptions & options)
+      : PathNode("relay", options)
+  {
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+    auto callback =
+      [this](std_msgs::msg::String::UniquePtr msg) -> void
+      {
+        pub_->publish(std::move(msg));
+      };
+
+    // Create a publisher with a custom Quality of Service profile.
+    rclcpp::QoS qos(rclcpp::KeepLast(7));
+    sub_ = this->create_subscription<std_msgs::msg::String>("in", qos, callback);
+    pub_ = this->create_publisher<std_msgs::msg::String>("out", qos);
+  }
+
+private:
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
+};
+
+}  // namespace pathnode_sample
+
+RCLCPP_COMPONENTS_REGISTER_NODE(pathnode_sample::Relay)
