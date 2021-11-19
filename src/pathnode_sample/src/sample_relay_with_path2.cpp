d5189f318a0b70f76c02b5f6015406bfbb1c911d
y-okumura <y-okumura@isp.co.jp>
2021-11-19 11:18:18 +0900

    Reformat by uncrustify

--- src/pathnode_sample/src/sample_relay_with_path2.cpp
+++ src/pathnode_sample/src/sample_relay_with_path2.cpp
@@ -36,10 +36,10 @@ namespace pathnode_sample
 // TODO: define per path
 const std::string IS_FIRST = "is_first";
 const std::string PATH_VALID_MIN_SEC = "path_valid_min_sec";
-const std::string PATH_VALID_MIN_NS  = "path_valid_min_ns";
+const std::string PATH_VALID_MIN_NS = "path_valid_min_ns";
 const std::string PATH_VALID_MAX_SEC = "path_valid_max_sec";
-const std::string PATH_VALID_MAX_NS  = "path_valid_max_ns";
-const std::string WAIT_MSEC          = "wait_msec";
+const std::string PATH_VALID_MAX_NS = "path_valid_max_ns";
+const std::string WAIT_MSEC = "wait_msec";
 
 // Create a Talker class that subclasses the generic rclcpp::Node base class.
 // The main function below will instantiate the class as a ROS node.
@@ -47,15 +47,15 @@ class RelayWithPath2 : public pathnode::PathNode
 {
 public:
   explicit RelayWithPath2(const rclcpp::NodeOptions & options)
-      : PathNode("relay", options), path_name_("sample_path")
+  : PathNode("relay", options), path_name_("sample_path")
   {
     /* newly introduced variables for path */
     declare_parameter<bool>(IS_FIRST, false);
-    // 	whole seconds (valid values are >= 0)
+    //  whole seconds (valid values are >= 0)
     declare_parameter<int64_t>(PATH_VALID_MIN_SEC, (int64_t) 0);
     // nanoseconds (valid values are [0, 999999999])
     declare_parameter<int64_t>(PATH_VALID_MIN_NS, (int64_t)( 1 * 1000 * 1000));
-    // 	whole seconds (valid values are >= 0)
+    //  whole seconds (valid values are >= 0)
     declare_parameter<int64_t>(PATH_VALID_MAX_SEC, (int64_t) 0);
     // nanoseconds (valid values are [0, 999999999])
     declare_parameter<int64_t>(PATH_VALID_MAX_NS, (int64_t)(10 * 1000 * 1000));
@@ -73,14 +73,14 @@ public:
 
     // now we can define unique_ptr version
     auto callback_unique_ptr =
-        [this, wait_msec](std_msgs::msg::String::UniquePtr msg) -> void
+      [this, wait_msec](std_msgs::msg::String::UniquePtr msg) -> void
       {
         std::cout << "relay: " << msg->data << std::endl;
         pub_->publish(std::move(msg));
       };
 
     auto callback_const =
-        [this, wait_msec](const std_msgs::msg::String &msg) -> void
+      [this, wait_msec](const std_msgs::msg::String & msg) -> void
       {
         std::cout << "relay: " << msg.data << std::endl;
         pub_->publish(msg);
@@ -89,9 +89,9 @@ public:
 
 
     // TODO check range
-    auto valid_min_tv_sec  = get_parameter(PATH_VALID_MIN_SEC).get_value<int64_t>();
+    auto valid_min_tv_sec = get_parameter(PATH_VALID_MIN_SEC).get_value<int64_t>();
     auto valid_min_tv_nsec = get_parameter(PATH_VALID_MIN_NS).get_value<int64_t>();
-    auto valid_max_tv_sec  = get_parameter(PATH_VALID_MAX_SEC).get_value<int64_t>();
+    auto valid_max_tv_sec = get_parameter(PATH_VALID_MAX_SEC).get_value<int64_t>();
     auto valid_max_tv_nsec = get_parameter(PATH_VALID_MAX_NS).get_value<int64_t>();
 
     pathnode::PathNodeSubscriptionOptions path_node_options;
@@ -108,11 +108,11 @@ public:
     */
 
     sub_ = this->create_path_node_subscription<std_msgs::msg::String>(
-        path_node_options,
-        "in", qos, callback_unique_ptr);
+      path_node_options,
+      "in", qos, callback_unique_ptr);
   }
 
- private:
+private:
   std::string path_name_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
