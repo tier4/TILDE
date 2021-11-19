d35429a8561826e901df5e19b0f2418c44cbd958
y-okumura <y-okumura@isp.co.jp>
2021-11-19 10:39:54 +0900

    Reformat by uncrustify

--- src/pathnode/src/timing_advertise_publisher.cpp
+++ src/pathnode/src/timing_advertise_publisher.cpp
@@ -26,15 +26,15 @@ rclcpp::Time pathnode::get_timestamp(rclcpp::Time t, ...)
 }
 
 void TimingAdvertisePublisherBase::set_input_info(
-    const std::string &sub_topic,
-    const std::shared_ptr<const InputInfo> p)
+  const std::string & sub_topic,
+  const std::shared_ptr<const InputInfo> p)
 {
   input_infos_[sub_topic] = p;
 }
 
 void TimingAdvertisePublisherBase::set_explicit_input_info(
-    const std::string &sub_topic,
-    const rclcpp::Time &stamp)
+  const std::string & sub_topic,
+  const rclcpp::Time & stamp)
 {
   // TODO(y-okumura-isp) set me
   // explicit_input_infos_[sub_topic].sub_time;
@@ -42,13 +42,13 @@ void TimingAdvertisePublisherBase::set_explicit_input_info(
   explicit_input_infos_[sub_topic].header_stamp = stamp;
 }
 
-void TimingAdvertisePublisherBase::set_input_info(path_info_msg::msg::PubInfo &info_msg)
+void TimingAdvertisePublisherBase::set_input_info(path_info_msg::msg::PubInfo & info_msg)
 {
-  if(explicit_input_infos_.size() == 0) {
+  if (explicit_input_infos_.size() == 0) {
     info_msg.input_infos.resize(input_infos_.size());
 
     size_t i = 0;
-    for(const auto &[topic, input_info] : input_infos_) {
+    for (const auto &[topic, input_info] : input_infos_) {
       info_msg.input_infos[i].topic_name = topic;
       info_msg.input_infos[i].sub_time = input_info->sub_time;
       info_msg.input_infos[i].has_header_stamp = input_info->has_header_stamp;
@@ -59,7 +59,7 @@ void TimingAdvertisePublisherBase::set_input_info(path_info_msg::msg::PubInfo &i
     info_msg.input_infos.resize(explicit_input_infos_.size());
 
     size_t i = 0;
-    for(const auto &[topic, input_info] : explicit_input_infos_) {
+    for (const auto &[topic, input_info] : explicit_input_infos_) {
       info_msg.input_infos[i].topic_name = topic;
       info_msg.input_infos[i].sub_time = input_info.sub_time;
       info_msg.input_infos[i].has_header_stamp = input_info.has_header_stamp;
