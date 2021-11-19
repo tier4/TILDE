4bff9d213ff74e2d6e960c4c31746975e9be5cff
y-okumura <y-okumura@isp.co.jp>
2021-11-19 09:58:06 +0900

    Pass cpplint

--- src/pathnode/src/sub_timing_advertise_node.cpp
+++ src/pathnode/src/sub_timing_advertise_node.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <string>
+
 #include "pathnode/sub_timing_advertise_node.hpp"
 
 using pathnode::SubTimingAdvertiseNode;
