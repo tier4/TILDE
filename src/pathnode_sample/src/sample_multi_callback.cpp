25fbd632ff71f4e94c5b30ed11339f7fecf7eb07
y-okumura <y-okumura@isp.co.jp>
2021-11-19 10:58:56 +0900

    Pass uncrustify

--- src/pathnode_sample/src/sample_multi_callback.cpp
+++ src/pathnode_sample/src/sample_multi_callback.cpp
@@ -30,23 +30,23 @@ using namespace std::chrono_literals;
 namespace pathnode_sample
 {
 
-const std::string REENTRANT      = "reentrant";
-const std::string SUB1_WAIT_MS   = "sub1_wait_ms";
-const std::string SUB2_WAIT_MS   = "sub2_wait_ms";
-const std::string SUB3_WAIT_MS   = "sub3_wait_ms";
+const std::string REENTRANT = "reentrant";
+const std::string SUB1_WAIT_MS = "sub1_wait_ms";
+const std::string SUB2_WAIT_MS = "sub2_wait_ms";
+const std::string SUB3_WAIT_MS = "sub3_wait_ms";
 const std::string TIMER1_WAIT_MS = "timer1_wait_ms";
 const std::string TIMER2_WAIT_MS = "timer2_wait_ms";
 const std::string TIMER1_INTERVAL_MS = "timer1_interval_ms";
 const std::string TIMER2_INTERVAL_MS = "timer2_interval_ms";
 
 /// multiple callback for checking callback blocking
-class SampleMultiCallback: public rclcpp::Node
+class SampleMultiCallback : public rclcpp::Node
 {
 public:
   explicit SampleMultiCallback(const rclcpp::NodeOptions & options)
-      : Node("sample_multi_callback", options)
+  : Node("sample_multi_callback", options)
   {
-    declare_parameter<bool>(REENTRANT,       false);
+    declare_parameter<bool>(REENTRANT, false);
     declare_parameter<int64_t>(SUB1_WAIT_MS, (int64_t) 0);
     declare_parameter<int64_t>(SUB2_WAIT_MS, (int64_t) 0);
     declare_parameter<int64_t>(SUB3_WAIT_MS, (int64_t) 0);
@@ -56,13 +56,13 @@ public:
     declare_parameter<int64_t>(TIMER2_INTERVAL_MS, (int64_t) 1000);
 
 
-    if(get_parameter(REENTRANT).get_value<bool>()) {
+    if (get_parameter(REENTRANT).get_value<bool>()) {
       callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
     }
 
-    int64_t sub1_wait_ms   = get_parameter(SUB1_WAIT_MS).get_value<int64_t>();
-    int64_t sub2_wait_ms   = get_parameter(SUB2_WAIT_MS).get_value<int64_t>();
-    int64_t sub3_wait_ms   = get_parameter(SUB3_WAIT_MS).get_value<int64_t>();
+    int64_t sub1_wait_ms = get_parameter(SUB1_WAIT_MS).get_value<int64_t>();
+    int64_t sub2_wait_ms = get_parameter(SUB2_WAIT_MS).get_value<int64_t>();
+    int64_t sub3_wait_ms = get_parameter(SUB3_WAIT_MS).get_value<int64_t>();
     int64_t timer1_wait_ms = get_parameter(TIMER1_WAIT_MS).get_value<int64_t>();
     int64_t timer2_wait_ms = get_parameter(TIMER2_WAIT_MS).get_value<int64_t>();
     int64_t timer1_interval_ms = get_parameter(TIMER1_INTERVAL_MS).get_value<int64_t>();
@@ -74,58 +74,60 @@ public:
     pub1_ = create_publisher<std_msgs::msg::String>("pub1", 1);
 
     auto sub1_callback =
-        [this, sub1_wait_ms](const std_msgs::msg::String &msg) -> void
-        {
-          std::cout << get_name() << ":sub1_callback " << msg.data << std::endl;
-          sleep("sub1_callback", sub1_wait_ms);
-          pub1_->publish(msg);
-        };
+      [this, sub1_wait_ms](const std_msgs::msg::String & msg) -> void
+      {
+        std::cout << get_name() << ":sub1_callback " << msg.data << std::endl;
+        sleep("sub1_callback", sub1_wait_ms);
+        pub1_->publish(msg);
+      };
     sub1_ = create_subscription<std_msgs::msg::String>(
-        "sub1", 3, sub1_callback, sub_options);
+      "sub1", 3, sub1_callback, sub_options);
 
     pub2_ = create_publisher<std_msgs::msg::String>("pub2", 1);
     auto sub2_callback =
-        [this, sub2_wait_ms](const std_msgs::msg::String &msg) -> void
-        {
-          std::cout << get_name() << "sub2_callback " << msg.data << std::endl;
-          sleep("sub2_callback", sub2_wait_ms);
-          pub2_->publish(msg);
-        };
+      [this, sub2_wait_ms](const std_msgs::msg::String & msg) -> void
+      {
+        std::cout << get_name() << "sub2_callback " << msg.data << std::endl;
+        sleep("sub2_callback", sub2_wait_ms);
+        pub2_->publish(msg);
+      };
     sub2_ = create_subscription<std_msgs::msg::String>(
-        "sub2", 3, sub2_callback, sub_options);
+      "sub2", 3, sub2_callback, sub_options);
 
     pub3_ = create_publisher<std_msgs::msg::String>("pub3", 1);
     auto sub3_callback =
-        [this, sub3_wait_ms](const std_msgs::msg::String &msg) -> void
-        {
-          std::cout << get_name() << "sub3_callback " << msg.data << std::endl;
-          sleep("sub3_callback", sub3_wait_ms);
-          pub3_->publish(msg);
-        };
+      [this, sub3_wait_ms](const std_msgs::msg::String & msg) -> void
+      {
+        std::cout << get_name() << "sub3_callback " << msg.data << std::endl;
+        sleep("sub3_callback", sub3_wait_ms);
+        pub3_->publish(msg);
+      };
     sub3_ = create_subscription<std_msgs::msg::String>(
-        "sub3", 3, sub3_callback, sub_options);
+      "sub3", 3, sub3_callback, sub_options);
 
     timer1_pub_ = create_publisher<std_msgs::msg::String>("timer_pub1", 1);
-    timer1_ = create_wall_timer(std::chrono::duration<int64_t, std::milli>(timer1_interval_ms),
-                                [this, timer1_wait_ms]() -> void
-                                {
-                                  sleep("timer1_callback", timer1_wait_ms);
-                                  std_msgs::msg::String msg;
-                                  msg.data = "timer1_callback";
-                                  timer1_pub_->publish(msg);
-                                },
-                                callback_group_);
+    timer1_ = create_wall_timer(
+      std::chrono::duration<int64_t, std::milli>(timer1_interval_ms),
+      [this, timer1_wait_ms]() -> void
+      {
+        sleep("timer1_callback", timer1_wait_ms);
+        std_msgs::msg::String msg;
+        msg.data = "timer1_callback";
+        timer1_pub_->publish(msg);
+      },
+      callback_group_);
 
     timer2_pub_ = create_publisher<std_msgs::msg::String>("timer_pub2", 1);
-    timer2_ = create_wall_timer(std::chrono::duration<int64_t, std::milli>(timer2_interval_ms),
-                                [this, timer2_wait_ms]() -> void
-                                {
-                                  sleep("timer2_callback", timer2_wait_ms);
-                                  std_msgs::msg::String msg;
-                                  msg.data = "timer2_callback";
-                                  timer2_pub_->publish(msg);
-                                },
-                                callback_group_);
+    timer2_ = create_wall_timer(
+      std::chrono::duration<int64_t, std::milli>(timer2_interval_ms),
+      [this, timer2_wait_ms]() -> void
+      {
+        sleep("timer2_callback", timer2_wait_ms);
+        std_msgs::msg::String msg;
+        msg.data = "timer2_callback";
+        timer2_pub_->publish(msg);
+      },
+      callback_group_);
   }
 
 private:
@@ -143,8 +145,9 @@ private:
 
   rclcpp::CallbackGroup::SharedPtr callback_group_;
 
-  void sleep(const std::string &name,
-             int64_t ms)
+  void sleep(
+    const std::string & name,
+    int64_t ms)
   {
     std::cout << get_name() << ":" << name << " start sleep " << ms << std::endl;
     std::this_thread::sleep_for(std::chrono::duration<int64_t, std::milli>(ms));
