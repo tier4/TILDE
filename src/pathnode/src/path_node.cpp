d967a68bc334ff5b69e895f70fae73ac699bca92
y-okumura <y-okumura@isp.co.jp>
2021-11-19 10:39:09 +0900

    Reformat by uncrustify

--- src/pathnode/src/path_node.cpp
+++ src/pathnode/src/path_node.cpp
@@ -23,31 +23,31 @@ using pathnode::PathNodeSubscriptionOptions;
 using pathnode::PathNodeInfo;
 using pathnode::PathNode;
 
-PathNodeSubscriptionOptions::PathNodeSubscriptionOptions():
-    valid_min_(0, 0), valid_max_(0, 0)
+PathNodeSubscriptionOptions::PathNodeSubscriptionOptions()
+: valid_min_(0, 0), valid_max_(0, 0)
 {
 }
 
-PathNodeInfo::PathNodeInfo():
-    CLOCK_TYPE(RCL_SYSTEM_TIME),
-    valid_min_(0, 0), valid_max_(0, 0)
-{
-}
-
-PathNode::PathNode(
-    const std::string & node_name,
-    const rclcpp::NodeOptions & options):
-    Node(node_name, options),
-    CLOCK_TYPE(RCL_SYSTEM_TIME)
+PathNodeInfo::PathNodeInfo()
+: CLOCK_TYPE(RCL_SYSTEM_TIME),
+  valid_min_(0, 0), valid_max_(0, 0)
 {
 }
 
 PathNode::PathNode(
-    const std::string & node_name,
-    const std::string & namespace_,
-    const rclcpp::NodeOptions & options):
-    Node(node_name, namespace_, options),
-    CLOCK_TYPE(RCL_SYSTEM_TIME)
+  const std::string & node_name,
+  const rclcpp::NodeOptions & options)
+: Node(node_name, options),
+  CLOCK_TYPE(RCL_SYSTEM_TIME)
+{
+}
+
+PathNode::PathNode(
+  const std::string & node_name,
+  const std::string & namespace_,
+  const rclcpp::NodeOptions & options)
+: Node(node_name, namespace_, options),
+  CLOCK_TYPE(RCL_SYSTEM_TIME)
 {
 }
 
@@ -56,13 +56,13 @@ PathNode::~PathNode()
 }
 
 void PathNode::setup_path(
-    const PathNodeSubscriptionOptions &path_node_options)
+  const PathNodeSubscriptionOptions & path_node_options)
 {
-  const auto& path_name = path_node_options.path_name_;
+  const auto & path_name = path_node_options.path_name_;
 
   std::lock_guard lock{path_node_info_map_mutex_};
   // if path_info already exists, then do nothing
-  if(path_node_info_map_.find(path_name) != path_node_info_map_.end()) {
+  if (path_node_info_map_.find(path_name) != path_node_info_map_.end()) {
     return;
   }
 
@@ -76,40 +76,40 @@ void PathNode::setup_path(
   info->valid_min_ = path_node_options.valid_min_;
   info->valid_max_ = path_node_options.valid_max_;
 
-  if(info->is_first_) {
+  if (info->is_first_) {
     info->pub_ = this->create_publisher<path_info_msg::msg::PathInfo>(path_name + "_info", qos);
   } else {
     auto path_info_callback =
-        [this](path_info_msg::msg::PathInfo::UniquePtr msg) -> void
-        {
-          std::lock_guard lock{path_node_info_map_mutex_};
-          auto info_it = path_node_info_map_.find(msg->path_name);
-          if(info_it == path_node_info_map_.end()) {
-            std::cout << "path_info not found: " << msg->path_name << std::endl;
-            return;
-          }
-          auto info = info_it->second;
-          info->path_tickets_.insert(rclcpp::Time(msg->path_start, info->CLOCK_TYPE));
-        };
+      [this](path_info_msg::msg::PathInfo::UniquePtr msg) -> void
+      {
+        std::lock_guard lock{path_node_info_map_mutex_};
+        auto info_it = path_node_info_map_.find(msg->path_name);
+        if (info_it == path_node_info_map_.end()) {
+          std::cout << "path_info not found: " << msg->path_name << std::endl;
+          return;
+        }
+        auto info = info_it->second;
+        info->path_tickets_.insert(rclcpp::Time(msg->path_start, info->CLOCK_TYPE));
+      };
     info->sub_ = this->create_subscription<path_info_msg::msg::PathInfo>(
-        path_name + "_info", qos, path_info_callback);
+      path_name + "_info", qos, path_info_callback);
   }
 
   std::cout << "create_path_info: " << path_name << std::endl;
   path_node_info_map_[path_name] = info;
 }
 
-void PathNode::on_pathed_subscription(const std::string &path_name)
+void PathNode::on_pathed_subscription(const std::string & path_name)
 {
   std::lock_guard lock{path_node_info_map_mutex_};
   auto info_it = this->path_node_info_map_.find(path_name);
-  if(info_it == this->path_node_info_map_.end()) {
+  if (info_it == this->path_node_info_map_.end()) {
     return;
   }
 
-  auto &info = info_it->second;
+  auto & info = info_it->second;
 
-  if(!info->is_first_) {
+  if (!info->is_first_) {
     return;
   }
 
@@ -121,29 +121,29 @@ void PathNode::on_pathed_subscription(const std::string &path_name)
   info->pub_->publish(std::move(m));
 }
 
-bool PathNode::pop_path_start_time(const std::string& path, rclcpp::Time &out)
+bool PathNode::pop_path_start_time(const std::string & path, rclcpp::Time & out)
 {
   std::lock_guard lock{path_node_info_map_mutex_};
   bool ret = false;
   auto path_node_it = path_node_info_map_.find(path);
-  if(path_node_it == path_node_info_map_.end()) {
-    std::cout << "pop_path_start_time cannot find path_node_info: " << std::endl;;
+  if (path_node_it == path_node_info_map_.end()) {
+    std::cout << "pop_path_start_time cannot find path_node_info: " << std::endl;
     return ret;
   }
   auto info = path_node_it->second;
 
-  auto &tickets = info->path_tickets_;
+  auto & tickets = info->path_tickets_;
 
   auto nw = now();
-  for(auto it = tickets.begin(); it != tickets.end();) {
+  for (auto it = tickets.begin(); it != tickets.end(); ) {
     // if ticket is too old, remove it
-    if(*it + info->valid_max_ < nw) {
+    if (*it + info->valid_max_ < nw) {
       std::cout << "pop_path_start_time erase: " << it->nanoseconds() << std::endl;
       // TODO(y-okumura-isp): silently erase? Is it bettter to notify that there is too old ticket?
       it = tickets.erase(it);
       continue;
     }
-    if(*it - info->valid_min_ < nw && nw < *it + info->valid_max_) {
+    if (*it - info->valid_min_ < nw && nw < *it + info->valid_max_) {
       ret = true;
       out = *it;
       it = tickets.erase(it);
@@ -155,31 +155,31 @@ bool PathNode::pop_path_start_time(const std::string& path, rclcpp::Time &out)
   return ret;
 }
 
-rclcpp::Duration PathNode::get_path_valid_min(const std::string &path)
+rclcpp::Duration PathNode::get_path_valid_min(const std::string & path)
 {
   // TODO(y-okumura-isp): need lock? initialize once and read only access
   // std::lock_guard lock{path_node_info_map_mutex_};
   auto path_node_it = path_node_info_map_.find(path);
-  if(path_node_it == path_node_info_map_.end()) {
-    std::cout << "get_path_valid_min: cannot find path_node_info: " << path << std::endl;;
+  if (path_node_it == path_node_info_map_.end()) {
+    std::cout << "get_path_valid_min: cannot find path_node_info: " << path << std::endl;
     return rclcpp::Duration(0, 0);
   }
 
-  auto &info = path_node_it->second;
+  auto & info = path_node_it->second;
   return info->valid_min_;
 }
 
-rclcpp::Duration PathNode::get_path_valid_max(const std::string &path)
+rclcpp::Duration PathNode::get_path_valid_max(const std::string & path)
 {
   // TODO(y-okumura-isp): need lock? initialize once and read only access
   // std::lock_guard lock{path_node_info_map_mutex_};
   auto path_node_it = path_node_info_map_.find(path);
-  if(path_node_it == path_node_info_map_.end()) {
-    std::cout << "get_path_valid_max: cannot find path_node_info: " << path << std::endl;;
+  if (path_node_it == path_node_info_map_.end()) {
+    std::cout << "get_path_valid_max: cannot find path_node_info: " << path << std::endl;
     return rclcpp::Duration(0, 0);
   }
 
-  auto &info = path_node_it->second;
+  auto & info = path_node_it->second;
   return info->valid_max_;
 }
 
