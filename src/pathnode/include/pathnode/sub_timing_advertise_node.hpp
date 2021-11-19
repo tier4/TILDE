9acdbe47bf258c8ef74b8daeabdbd512f724e4ae
y-okumura <y-okumura@isp.co.jp>
2021-11-19 10:32:22 +0900

    Reformat by uncrustify

--- src/pathnode/include/pathnode/sub_timing_advertise_node.hpp
+++ src/pathnode/include/pathnode/sub_timing_advertise_node.hpp
@@ -35,7 +35,8 @@
 
 namespace pathnode
 {
-template<class> inline constexpr bool always_false_v = false;
+template<class>
+inline constexpr bool always_false_v = false;
 
 /// PoC of `every sub talks sub timing`
 class SubTimingAdvertiseNode : public rclcpp::Node
@@ -88,7 +89,7 @@ public:
     rclcpp::SubscriptionOptionsWithAllocator<AllocatorT>(),
     typename MessageMemoryStrategyT::SharedPtr msg_mem_strat = (
       MessageMemoryStrategyT::create_default()
-    ))
+  ))
   {
     using rclcpp::node_interfaces::get_node_topics_interface;
     auto node_topics_interface = get_node_topics_interface(this);
@@ -97,85 +98,85 @@ public:
     auto topic_info_name = resolved_topic_name + "/info/sub";
 
     auto topic_info_pub = create_publisher<path_info_msg::msg::TopicInfo>(
-        topic_info_name,
-        rclcpp::QoS(1));
+      topic_info_name,
+      rclcpp::QoS(1));
     topic_info_pubs_[topic_info_name] = topic_info_pub;
     seqs_[topic_info_name] = 0;
 
-    auto main_topic_callback
-        = [this, resolved_topic_name, topic_info_name, callback](
-            CallbackArgT msg,
-            const rclcpp::MessageInfo &info) -> void
-          {
-            // publish subscription timing
-            auto minfo = info.get_rmw_message_info();
+    auto main_topic_callback =
+      [this, resolved_topic_name, topic_info_name, callback](
+      CallbackArgT msg,
+      const rclcpp::MessageInfo & info) -> void
+      {
+        // publish subscription timing
+        auto minfo = info.get_rmw_message_info();
 
-            auto m = std::make_unique<path_info_msg::msg::TopicInfo>();
-            auto &seq = seqs_[topic_info_name];
-            m->seq = seq;
-            seq++;
-            m->node_fqn = get_fully_qualified_name();
-            m->topic_name = resolved_topic_name;
-            for(size_t i = 0; i < RMW_GID_STORAGE_SIZE; i++) {
-              m->publisher_gid[i] = minfo.publisher_gid.data[i];
-            }
-            m->callback_start = now();
-            topic_info_pubs_[topic_info_name]->publish(std::move(m));
+        auto m = std::make_unique<path_info_msg::msg::TopicInfo>();
+        auto & seq = seqs_[topic_info_name];
+        m->seq = seq;
+        seq++;
+        m->node_fqn = get_fully_qualified_name();
+        m->topic_name = resolved_topic_name;
+        for (size_t i = 0; i < RMW_GID_STORAGE_SIZE; i++) {
+          m->publisher_gid[i] = minfo.publisher_gid.data[i];
+        }
+        m->callback_start = now();
+        topic_info_pubs_[topic_info_name]->publish(std::move(m));
 
-            // prepare InputInfo
-            using ConstRef = const MessageT &;
-            using UniquePtr = std::unique_ptr<MessageT, MessageDeleter>;
-            using SharedConstPtr = std::shared_ptr<const MessageT>;
-            using ConstRefSharedConstPtr = const std::shared_ptr<const MessageT> &;
-            using SharedPtr = std::shared_ptr<MessageT>;
+        // prepare InputInfo
+        using ConstRef = const MessageT &;
+        using UniquePtr = std::unique_ptr<MessageT, MessageDeleter>;
+        using SharedConstPtr = std::shared_ptr<const MessageT>;
+        using ConstRefSharedConstPtr = const std::shared_ptr<const MessageT>&;
+        using SharedPtr = std::shared_ptr<MessageT>;
 
-            rclcpp::Time header_stamp;
-            rclcpp::Time t(0, 100, this->now().get_clock_type());
+        rclcpp::Time header_stamp;
+        rclcpp::Time t(0, 100, this->now().get_clock_type());
 
-            using S = std::decay_t<decltype(msg)>;
-            if constexpr (std::is_same_v<S, ConstRef>) {
-              // std::cout << "visit: ConstRef\n";
-              header_stamp = Process<MessageT>::get_timestamp3(t, &msg);
-            } else if constexpr (std::is_same_v<S, UniquePtr>) {
-              // std::cout << "visit: UniquePtr\n";
-              header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
-            } else if constexpr (std::is_same_v<S, SharedConstPtr>) {
-              // std::cout << "visit: SharedConstPtr\n";
-              header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
-            } else if constexpr (std::is_same_v<S, ConstRefSharedConstPtr>) {
-              // std::cout << "visit: ConstRefSharedPtr\n";
-              header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
-            } else if constexpr (std::is_same_v<S, SharedPtr>) {
-              // std::cout << "visit: SharedPtr\n";
-              header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
-            } else {
-              static_assert(always_false_v<S>, "non-exhaustive visitor!");
-            }
+        using S = std::decay_t<decltype(msg)>;
+        if constexpr (std::is_same_v<S, ConstRef>) {
+          // std::cout << "visit: ConstRef\n";
+          header_stamp = Process<MessageT>::get_timestamp3(t, &msg);
+        } else if constexpr (std::is_same_v<S, UniquePtr>) {
+          // std::cout << "visit: UniquePtr\n";
+          header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
+        } else if constexpr (std::is_same_v<S, SharedConstPtr>) {
+          // std::cout << "visit: SharedConstPtr\n";
+          header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
+        } else if constexpr (std::is_same_v<S, ConstRefSharedConstPtr>) {
+          // std::cout << "visit: ConstRefSharedPtr\n";
+          header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
+        } else if constexpr (std::is_same_v<S, SharedPtr>) {
+          // std::cout << "visit: SharedPtr\n";
+          header_stamp = Process<MessageT>::get_timestamp3(t, msg.get());
+        } else {
+          static_assert(always_false_v<S>, "non-exhaustive visitor!");
+        }
 
-            auto input_info = std::make_shared<InputInfo>();
+        auto input_info = std::make_shared<InputInfo>();
 
-            input_info->sub_time = now();
-            if(header_stamp != t) {
-              input_info->has_header_stamp = true;
-              input_info->header_stamp = header_stamp;
-            }
+        input_info->sub_time = now();
+        if (header_stamp != t) {
+          input_info->has_header_stamp = true;
+          input_info->header_stamp = header_stamp;
+        }
 
-            // TODO(y-okumura-isp): consider race condition in multi threaded executor.
-            // i.e. subA comes when subB callback which uses topicA is running
-            for(auto &[topic, tap] : timing_advertise_pubs_) {
-              tap->set_input_info(resolved_topic_name, input_info);
-            }
+        // TODO(y-okumura-isp): consider race condition in multi threaded executor.
+        // i.e. subA comes when subB callback which uses topicA is running
+        for (auto &[topic, tap] : timing_advertise_pubs_) {
+          tap->set_input_info(resolved_topic_name, input_info);
+        }
 
-            // finally, call original function
-            callback(std::forward<CallbackArgT>(msg));
-          };
+        // finally, call original function
+        callback(std::forward<CallbackArgT>(msg));
+      };
 
     return create_subscription<MessageT>(
-        topic_name,
-        qos,
-        main_topic_callback,
-        options,
-        msg_mem_strat);
+      topic_name,
+      qos,
+      main_topic_callback,
+      options,
+      msg_mem_strat);
   }
 
   template<
@@ -194,10 +195,10 @@ public:
     auto pub = create_publisher<MessageT, AllocatorT, PublisherT>(topic_name, qos, options);
     auto info_topic = std::string(pub->get_topic_name()) + "/info/pub";
     auto info_pub = create_publisher<TimingAdvertisePublisherBase::InfoMsg>(
-        info_topic, rclcpp::QoS(1), options);
+      info_topic, rclcpp::QoS(1), options);
 
     auto ta_pub = std::make_shared<TimingAdvertisePublisherT>(
-        info_pub, pub, get_fully_qualified_name());
+      info_pub, pub, get_fully_qualified_name());
     timing_advertise_pubs_[info_topic] = ta_pub;
     return ta_pub;
   }
