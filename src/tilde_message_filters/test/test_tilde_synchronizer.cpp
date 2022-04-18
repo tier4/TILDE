c0a1356450170cbc705f9e0f937f5d4cae0c4c14
y-okumura <y-okumura@isp.co.jp>
2022-04-18 14:43:49 +0900

    [wip] Use test fixtures on UT

--- src/tilde_message_filters/test/test_tilde_synchronizer.cpp
+++ src/tilde_message_filters/test/test_tilde_synchronizer.cpp
@@ -22,11 +22,11 @@
 #include "tilde_message_filters/tilde_subscriber.h"
 #include "tilde_message_filters/tilde_synchronizer.h"
 
+using namespace tilde;
 using namespace message_filters;
 using namespace tilde_message_filters;
 
 using Node = rclcpp::Node;
-using TildeNode = tilde::TildeNode;
 using PointCloud2 = sensor_msgs::msg::PointCloud2;
 using PointCloud2Ptr = std::shared_ptr<PointCloud2>;
 using PointCloud2ConstPtr = std::shared_ptr<PointCloud2 const>;
@@ -37,15 +37,70 @@ using TimeMsg = builtin_interfaces::msg::Time;
 
 class TestSynchronizer : public ::testing::Test
 {
+public:
   void SetUp() override
   {
     rclcpp::init(0, nullptr);
+
+    options.append_parameter_override("use_sim_time", true);
+    qos.keep_last(10);
+
+    // setup pub node
+    pub_node = std::make_shared<Node>("pub_node", options);
+    pub1 = pub_node->create_publisher<PointCloud2>("in1", qos);
+    pub2 = pub_node->create_publisher<PointCloud2>("in2", qos);
+    clock_pub = pub_node->create_publisher<Clock>("/clock", qos);
+
+    // setup sub node
+    sub_node = std::make_shared<TildeNode>("sub_node", options);
+    sub1.subscribe(sub_node, "in1", qos.get_rmw_qos_profile());
+    sub2.subscribe(sub_node, "in2", qos.get_rmw_qos_profile());
+    out_pub = sub_node->create_tilde_publisher<PointCloud2>("out", qos);
+
+    // setup val node
+    val_node = std::make_shared<Node>("val_node", options);
   }
 
   void TearDown() override
   {
     rclcpp::shutdown();
   }
+
+  Clock send_clock(int32_t sec, uint32_t nsec)
+  {
+    Clock clock_msg;
+    clock_msg.clock.sec = sec;
+    clock_msg.clock.nanosec = nsec;
+    clock_pub->publish(clock_msg);
+    return clock_msg;
+  };
+
+  Clock send_clock_and_spin(int32_t sec, uint32_t nsec)
+  {
+    auto clock = send_clock(sec, nsec);
+    spin();
+    return clock;
+  };
+
+  void spin()
+  {
+    rclcpp::spin_some(pub_node);
+    rclcpp::spin_some(sub_node);
+    rclcpp::spin_some(val_node);
+  }
+
+  rclcpp::NodeOptions options;
+  rclcpp::QoS qos{10};
+
+  std::shared_ptr<Node> pub_node;
+  std::shared_ptr<rclcpp::Publisher<PointCloud2>> pub1, pub2;
+  std::shared_ptr<rclcpp::Publisher<Clock>> clock_pub;
+
+  std::shared_ptr<TildeNode> sub_node;
+  TildeSubscriber<PointCloud2> sub1, sub2;
+  std::shared_ptr<TildePublisher<PointCloud2>> out_pub;
+
+  std::shared_ptr<Node> val_node;
 };
 
 void EXPECT_CLOCK(rclcpp::Time time, int sec, uint32_t nsec)
@@ -57,24 +112,6 @@ void EXPECT_CLOCK(rclcpp::Time time, int sec, uint32_t nsec)
 
 
 TEST_F(TestSynchronizer, exact_policy_2msgs) {
-  rclcpp::NodeOptions options;
-  options.append_parameter_override("use_sim_time", true);
-
-  rclcpp::QoS qos(rclcpp::KeepLast(10));
-
-  // setup pub_node
-  auto pub_node = std::make_shared<Node>("pub_node", options);
-  auto pub1 = pub_node->create_publisher<PointCloud2>("in1", qos);
-  auto pub2 = pub_node->create_publisher<PointCloud2>("in2", qos);
-  auto clock_pub = pub_node->create_publisher<Clock>("/clock", qos);
-
-  // setup sub_node
-  auto sub_node = std::make_shared<TildeNode>("sub_node", options);
-  TildeSubscriber<PointCloud2> sub1, sub2;
-  sub1.subscribe(sub_node, "in1", qos.get_rmw_qos_profile());
-  sub2.subscribe(sub_node, "in2", qos.get_rmw_qos_profile());
-  auto out_pub = sub_node->create_tilde_publisher<PointCloud2>("out", qos);
-
   // setup sub_node synchronizer
   using SyncPolicy = sync_policies::ExactTime<PointCloud2, PointCloud2>;
   using Sync = TildeSynchronizer<SyncPolicy>;
@@ -82,7 +119,7 @@ TEST_F(TestSynchronizer, exact_policy_2msgs) {
 
   bool sync_callback_called = false;
   sync->registerCallback(
-      [&sync_callback_called, out_pub](const PointCloud2ConstPtr &msg1,
+      [this, &sync_callback_called](const PointCloud2ConstPtr &msg1,
          const PointCloud2ConstPtr &msg2) -> void
       {
         (void) msg1;
@@ -92,11 +129,10 @@ TEST_F(TestSynchronizer, exact_policy_2msgs) {
       });
 
   // validation node
-  auto val_node = std::make_shared<Node>("val_node", options);
   bool val_callback_called = false;
   auto val_sub = val_node->create_subscription<PubInfo>(
       "out/info/pub", qos,
-      [&val_callback_called](PubInfoPtr pi) -> void
+      [this, &val_callback_called](PubInfoPtr pi) -> void
       {
         val_callback_called = true;
         EXPECT_EQ(pi->output_info.header_stamp.sec, 123);
@@ -137,28 +173,12 @@ TEST_F(TestSynchronizer, exact_policy_2msgs) {
         }
       });
 
-  // utilities
-  auto send_clock =
-      [clock_pub](int32_t sec, uint32_t nsec) -> void
-      {
-        Clock clock_msg;
-        clock_msg.clock.sec = sec;
-        clock_msg.clock.nanosec = nsec;
-        clock_pub->publish(clock_msg);
-      };
-
-  auto spin =
-      [pub_node, sub_node, val_node]() -> void
-      {
-        rclcpp::spin_some(pub_node);
-        rclcpp::spin_some(sub_node);
-        rclcpp::spin_some(val_node);
-      };
-
   // apply "/clock"
+  std::cout << "clock\n";
   send_clock(123, 456);
   spin();
   EXPECT_CLOCK(sub_node->now(), 123, 456u);
+  std::cout << "clock\n";
 
   // PointCloud2 message to publish
   PointCloud2 msg;
@@ -168,17 +188,20 @@ TEST_F(TestSynchronizer, exact_policy_2msgs) {
   msg.header.frame_id = 1;
   pub1->publish(msg);
   spin();
+  std::cout << "pub1\n";
 
   // update clock
   send_clock(124, 321);
   spin();
   EXPECT_CLOCK(sub_node->now(), 124, 321);
+  std::cout << "clock2\n";
 
   // pub2
   EXPECT_FALSE(val_callback_called);
   msg.header.frame_id = 2;
   pub2->publish(msg);
   spin();
+  std::cout << "pub2\n";
 
   // verify
   EXPECT_TRUE(sync_callback_called);
