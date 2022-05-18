189cef8b2410106ec1fa5beec7cf2a18d78039ba
y-okumura-isp <y-okumura@isp.co.jp>
2022-05-18 14:26:17 +0900

    Add tilde_deadline_detector_node

---
+++ src/tilde_deadline_detector/include/tilde_deadline_detector/tilde_deadline_detector_node.hpp
@@ -0,0 +1,46 @@
+// Copyright 2022 Research Institute of Systems Planning, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TILDE_DEADLINE_DETECTOR__TILDE_DEADLINE_DETECTOR_NODE_HPP_
+#define TILDE_DEADLINE_DETECTOR__TILDE_DEADLINE_DETECTOR_NODE_HPP_
+
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tilde_msg/msg/pub_info.hpp"
+
+namespace tilde_deadline_detector
+{
+class TildeDeadlineDetectorNode : public rclcpp::Node
+{
+public:
+  RCLCPP_PUBLIC
+  explicit TildeDeadlineDetectorNode(
+    const std::string & node_name,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  /// see corresponding rclcpp::Node constructor
+  RCLCPP_PUBLIC
+  explicit TildeDeadlineDetectorNode(
+    const std::string & node_name,
+    const std::string & namespace_,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  RCLCPP_PUBLIC
+  virtual ~TildeDeadlineDetectorNode();
+};
+
+}  // namespace tilde_deadline_detector
+
+#endif  // TILDE_DEADLINE_DETECTOR__TILDE_DEADLINE_DETECTOR_NODE_HPP_
