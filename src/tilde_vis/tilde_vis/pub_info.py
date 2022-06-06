22ee437c9525152be5e138756b46bf1bf652ce99
y-okumura-isp <y-okumura@isp.co.jp>
2022-06-06 15:44:46 +0900

    Pass flake8

--- src/tilde_vis/tilde_vis/pub_info.py
+++ src/tilde_vis/tilde_vis/pub_info.py
@@ -16,16 +16,17 @@ from rclpy.time import Time
 
 
 def time2str(t):
-    """
-    t: builtin_interfaces.msg.Time
-    """
-    return f"{t.sec}.{t.nanosec:09d}"
+    """Convert builtin_interfaces.msg.Time to string."""
+    return f'{t.sec}.{t.nanosec:09d}'
 
 
 class TopicInfo(object):
     def __init__(self, topic, pubsub_stamp, pubsub_stamp_steady,
                  has_stamp, stamp):
-        """
+        """Hold information similar to PubInfo in/out info.
+
+        Parameters
+        ----------
         topic: target topic [string]
         pubsub_stamp:
           when publish or subscription callback is called
@@ -35,6 +36,7 @@ class TopicInfo(object):
           [builtin_interfaces.mg.Time]
         has_stamp: whether main topic has header.stamp or not [bool]
         stamp: header.stamp [builtin_interfaces.msg.Time]
+
         """
         self.topic = topic
         self.pubsub_stamp = pubsub_stamp
@@ -43,23 +45,28 @@ class TopicInfo(object):
         self.stamp = stamp
 
     def __str__(self):
-        stamp_s = time2str(self.stamp) if self.has_stamp else "NA"
-        return f"TopicInfo(topic={self.topic}, stamp={stamp_s})"
+        stamp_s = time2str(self.stamp) if self.has_stamp else 'NA'
+        return f'TopicInfo(topic={self.topic}, stamp={stamp_s})'
 
 
 class PubInfo(object):
+    """Hold information similar to PubInfoMsg.
+
+    TODO(y-okumura-isp): Can we use PubInfoMsg directly?
+
     """
-    PubInfo
-      - out_info = TopicInfo
-      - in_infos = {topic_name => list of TopicInfo}
-    """
+
     def __init__(self, out_topic, pub_time, pub_time_steady,
                  has_stamp, out_stamp):
-        """
+        """Constructor.
+
+        Parameters
+        ----------
         out_topic: topic name
         pub_time: when publish main topic [builtin_interfaces.msg.Time]
         has_stamp: whether main topic has header.stamp [bool]
         out_stamp: main topic header.stamp [builtin_interfaces.msg.Time]
+
         """
         self.out_info = TopicInfo(out_topic, pub_time, pub_time_steady,
                                   has_stamp, out_stamp)
@@ -68,21 +75,25 @@ class PubInfo(object):
 
     def add_input_info(self, in_topic, sub_stamp, sub_stamp_steady,
                        has_stamp, stamp):
-        """
+        """Add input info.
+
+        Parameters
+        ----------
         in_topic: topic string
         has_stamp: bool
         stamp: header stamp [builtin_interfaces.msg.Time]
+
         """
         info = TopicInfo(in_topic, sub_stamp, sub_stamp_steady,
                          has_stamp, stamp)
         self.in_infos.setdefault(in_topic, []).append(info)
 
     def __str__(self):
-        s = "PubInfo: \n"
-        s += f"  out_info={self.out_info}\n"
+        s = 'PubInfo: \n'
+        s += f'  out_info={self.out_info}\n'
         for _, infos in self.in_infos.items():
             for info in infos:
-                s += f"  in_infos={info}\n"
+                s += f'  in_infos={info}\n'
         return s
 
     @property
@@ -91,7 +102,8 @@ class PubInfo(object):
 
     @staticmethod
     def fromMsg(pub_info_msg):
-        """
+        """Convert PubInfoMsg to PubInfo.
+
         Parameters
         ----------
         pub_info_msg: PubInfoMsg
@@ -99,6 +111,7 @@ class PubInfo(object):
         Returns
         -------
         PubInfo
+
         """
         output_info = pub_info_msg.output_info
         pub_info = PubInfo(output_info.topic_name,
@@ -116,12 +129,13 @@ class PubInfo(object):
 
 
 class PubInfos(object):
-    '''
-    topic vs PubInfo
+    """Hold topic vs PubInfo.
 
     We have double-key dictionary internally, i.e.
     we can get PubInfo by topic_vs_pubinfo[topic_name][stamp].
-    '''
+
+    """
+
     def __init__(self):
         # {topic => {stamp_str => PubInfo}}
         self.topic_vs_pubinfos = {}
@@ -139,19 +153,26 @@ class PubInfos(object):
         infos[out_stamp] = pubinfo
 
     def erase_until(self, stamp):
-        """
-        erase added pubinfo where out_stamp < stamp
+        """Erase added pubinfo where out_stamp < stamp.
+
+        Parameters
+        ----------
         stamp: builtin_interfaces.msg.Time
+
         """
         def time_ge(lhs, rhs):
-            """helper function to compare stamps i.e. self.topic_vs_pubinfos[*].keys().
+            """Compare time.
+
+            Helper function to compare stamps i.e.
+            self.topic_vs_pubinfos[*].keys().
 
             As stamps are string, it is not appropriate to compare as string.
             Builtin_msg.msg.Time does not implement `<=>`, we use rclpy.Time,
             althoght clock_type has no meaning.
+
             """
-            [lhs_sec, lhs_nsec] = map(lambda x: int(x), lhs.split("."))
-            [rhs_sec, rhs_nsec] = map(lambda x: int(x), rhs.split("."))
+            [lhs_sec, lhs_nsec] = map(lambda x: int(x), lhs.split('.'))
+            [rhs_sec, rhs_nsec] = map(lambda x: int(x), rhs.split('.'))
             lhs_time = Time(seconds=lhs_sec, nanoseconds=lhs_nsec)
             rhs_time = Time(seconds=rhs_sec, nanoseconds=rhs_nsec)
             return lhs_time <= rhs_time
@@ -172,20 +193,24 @@ class PubInfos(object):
         return list(self.topic_vs_pubinfos.keys())
 
     def stamps(self, topic):
-        """
-        return List[stamps]
-        """
+        """Get List[stamps]."""
         if topic not in self.topic_vs_pubinfos.keys():
             return []
 
         return list(self.topic_vs_pubinfos[topic].keys())
 
     def get(self, topic, stamp=None, idx=None):
-        """
+        """Get a PubInfo.
+
+        Parameters
+        ----------
         topic: str
         stamp: str such as 1618559286.884563157
 
-        return PubInfo or None
+        Return
+        ------
+        PubInfo or None
+
         """
         ret = None
         if topic not in self.topic_vs_pubinfos.keys():
@@ -201,7 +226,7 @@ class PubInfos(object):
 
         if idx is not None:
             if len(infos.keys()) <= idx:
-                print("args.idx too large, should be < {len(info.kens())}")
+                print('args.idx too large, should be < {len(info.kens())}')
             else:
                 key = sorted(infos.keys())[idx]
                 ret = infos[key]
@@ -209,9 +234,12 @@ class PubInfos(object):
         return ret
 
     def in_topics(self, topic):
-        """
-        gather input topics by ignoring stamps
-        return set of topic name
+        """Gather input topics by ignoring stamps.
+
+        Return
+        ------
+        set of topic name
+
         """
         ret = set()
 
