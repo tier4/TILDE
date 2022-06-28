0ea064a880238b7ea72d98c701be9ab5443c55c7
y-okumura-isp <y-okumura@isp.co.jp>
2022-06-28 11:03:59 +0900

    [wip] Implementing SteePublisher

---
+++ src/tilde/include/tilde/message_conversion.hpp
@@ -0,0 +1,42 @@
+// Copyright 2021 Research Institute of Systems Planning, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TILDE__MESSAGE_CONVERSION_HPP_
+#define TILDE__MESSAGE_CONVERSION_HPP_
+
+#include <tuple>
+
+#include "tilde/message_conversion_detail.hpp"
+
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "tilde_msg/msg/stee_point_cloud2.hpp"
+
+namespace tilde
+{
+// define your type
+using TypeTable = std::tuple<
+  Pair<sensor_msgs::msg::PointCloud2, tilde_msg::msg::SteePointCloud2>
+  >;
+
+template<typename Key>
+struct _ConvertedType {
+  using type = typename Get<Key, TypeTable>::type;
+};
+
+template<typename Key>
+using ConvertedMessageType = typename _ConvertedType<Key>::type;
+
+}  // namespace tilde
+
+#endif  // TILDE__MESSAGE_CONVERSION_HPP_
---
+++ src/tilde/include/tilde/stee_publisher.hpp
@@ -0,0 +1,65 @@
+// Copyright 2021 Research Institute of Systems Planning, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TILDE__STEE_PUBLISHER_HPP_
+#define TILDE__STEE_PUBLISHER_HPP_
+
+#include <memory>
+
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/macros.hpp"
+
+#include "tilde/tilde_publisher.hpp"
+#include "tilde_msg/msg/stee_source.hpp"
+
+namespace tilde
+{
+
+template<typename MessageT,
+         typename ConvertedMessageT,
+         typename AllocatorT = std::allocator<void>>
+class SteePublisher : public TildePublisherBase
+{
+private:
+  using MessageAllocatorTraits = rclcpp::allocator::AllocRebind<MessageT, AllocatorT>;
+  using MessageAllocator = typename MessageAllocatorTraits::allocator_type;
+  using MessageDeleter = rclcpp::allocator::Deleter<MessageAllocator, MessageT>;
+  using PublisherT = rclcpp::Publisher<MessageT, AllocatorT>;
+  using ConvertedPublisherT = rclcpp::Publisher<ConvertedMessageT, AllocatorT>;
+
+public:
+  RCLCPP_SMART_PTR_DEFINITIONS(SteePublisher)
+
+  /// Default constructor
+  SteePublisher(
+    std::shared_ptr<PublisherT> pub,
+    std::shared_ptr<ConvertedPublisherT> converted_pub,
+    const std::string & node_fqn,
+    std::shared_ptr<rclcpp::Clock> clock,
+    std::shared_ptr<rclcpp::Clock> steady_clock)
+  : TildePublisherBase(clock, steady_clock, node_fqn),
+    pub_(pub),
+    converted_pub_(converted_pub)
+  {
+  }
+
+private:
+  std::shared_ptr<PublisherT> pub_;
+  std::shared_ptr<ConvertedPublisherT> converted_pub_;
+};
+
+}  // namespace tilde
+
+#endif  // TILDE__STEE_PUBLISHER_HPP_
