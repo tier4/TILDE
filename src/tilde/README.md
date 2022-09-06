077532f30bd61c6dbab7610292f67e2fe2269a10
y-okumura-isp <y-okumura@isp.co.jp>
2022-09-06 16:09:41 +0900

    Add explicit API test pattern for TSAN

--- src/tilde/README.md
+++ src/tilde/README.md
@@ -1,3 +1,16 @@
 # Tilde
 
 see [doc](../../doc/README.md)
+
+## About test
+
+### test_stee_multithread.cpp
+
+Use this with TSAN.
+
+```
+colcon build --symlink-install --packages-select tilde --mixin tsan
+./build/tilde/test_stee_multithread
+```
+
+Check standard out and see TSAN message.
--- src/tilde/test/test_stee_multithread.cpp
+++ src/tilde/test/test_stee_multithread.cpp
@@ -46,9 +46,10 @@ public:
   }
 };
 
+#if 0
 TEST_F(TestSteeNode, stee_multi_thread) {
   rclcpp::NodeOptions options;
-  options.append_parameter_override("use_sim_time", true);
+  options.append_parameter_override("use_sim_time", false);
 
   // sensor node
   auto sensor_node = std::make_shared<SteeNode>("sensor_node", options);
@@ -96,6 +97,7 @@ TEST_F(TestSteeNode, stee_multi_thread) {
         for (int i=0; i<10; i++) {
           std::cout << "publish " << std::to_string(i) << std::endl;
           PointCloud2 msg;
+          msg.header.stamp.sec = i;
           msg.header.frame_id = std::to_string(i);
           sensor_pub1->publish(msg);
           sensor_pub2->publish(msg);
@@ -118,7 +120,7 @@ TEST_F(TestSteeNode, stee_multi_thread) {
 // use rclcpp::Node case, for comparison
 TEST_F(TestSteeNode, non_stee_multi_thread) {
   rclcpp::NodeOptions options;
-  options.append_parameter_override("use_sim_time", true);
+  options.append_parameter_override("use_sim_time", false);
 
   // sensor node
   auto sensor_node = std::make_shared<Node>("sensor_node", options);
@@ -166,6 +168,85 @@ TEST_F(TestSteeNode, non_stee_multi_thread) {
         for (int i=0; i<10; i++) {
           std::cout << "publish " << std::to_string(i) << std::endl;
           PointCloud2 msg;
+          msg.header.stamp.sec = i;
+          msg.header.frame_id = std::to_string(i);
+          sensor_pub1->publish(msg);
+          sensor_pub2->publish(msg);
+          std::this_thread::sleep_for(500ms);
+        }
+      });
+
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(main_node);
+  std::thread main_node_thread(
+      [&executor]() -> void
+      {
+        executor.spin();
+      });
+  sender_thread.join();
+  executor.cancel();
+  main_node_thread.join();
+}
+#endif
+
+/// test for explicit API
+TEST_F(TestSteeNode, stee_multi_thread_with_explicit_api) {
+  rclcpp::NodeOptions options;
+  options.append_parameter_override("use_sim_time", false);
+
+  // sensor node
+  auto sensor_node = std::make_shared<SteeNode>("sensor_node", options);
+  auto sensor_pub1 = sensor_node->create_stee_publisher<PointCloud2>("topic1", 1);
+  auto sensor_pub2 = sensor_node->create_stee_publisher<PointCloud2>("topic2", 1);
+
+  // main node
+  auto main_node = std::make_shared<SteeNode>("stee_node", options);
+  auto main_pub = main_node->create_stee_publisher<PointCloud2>("main", 1);
+
+  auto checker_node = std::make_shared<rclcpp::Node>("checker_node");
+
+  auto cb1 = main_node->create_callback_group(
+      rclcpp::CallbackGroupType::Reentrant);
+  auto cb2 = main_node->create_callback_group(
+      rclcpp::CallbackGroupType::Reentrant);
+
+  auto sub_opt1 = rclcpp::SubscriptionOptions();
+  sub_opt1.callback_group = cb1;
+
+  auto sub_opt2 = rclcpp::SubscriptionOptions();
+  sub_opt2.callback_group = cb2;
+
+  auto main_sub1 = main_node->create_stee_subscription<PointCloud2>(
+    "topic1", 1,
+    [&main_pub](PointCloud2::UniquePtr msg) -> void
+    {
+      std::cout << "topic1" << std::endl;
+      main_pub->add_explicit_input_info(
+          "/topic1",
+          msg->header.stamp);
+      main_pub->publish(std::move(msg));
+    },
+    sub_opt1);
+
+  auto main_sub2 = main_node->create_stee_subscription<PointCloud2>(
+    "/topic2", 1,
+    [&main_pub](PointCloud2::UniquePtr msg) -> void
+    {
+      std::cout << "topic2" << std::endl;
+      main_pub->add_explicit_input_info(
+          "topic2",
+          msg->header.stamp);
+      main_pub->publish(std::move(msg));
+    },
+    sub_opt2);
+
+  std::thread sender_thread(
+      [&sensor_pub1, &sensor_pub2]() -> void
+      {
+        for (int i=0; i<10; i++) {
+          std::cout << "publish " << std::to_string(i) << std::endl;
+          PointCloud2 msg;
+          msg.header.stamp.sec = i;
           msg.header.frame_id = std::to_string(i);
           sensor_pub1->publish(msg);
           sensor_pub2->publish(msg);
